ab36033de662d01dd5c1f1a9b2fd1a2a10532050
Havva Fatma Özbay <havvafatma45@gmail.com>
2022-06-30 19:25:51 +0300

    Initial Commit

---
+++ STL Algorithms/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  STL Algorithms
+//
+//  Created by Havva Fatma Özbay on 30.06.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
