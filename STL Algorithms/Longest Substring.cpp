8e73a74b9e308dd581e6303f5cfd3e3422366014
Havva Fatma Özbay <havvafatma45@gmail.com>
2022-08-31 14:54:59 +0300

    //

---
+++ STL Algorithms/Longest Substring.cpp
@@ -0,0 +1,43 @@
+//
+//  main.cpp
+//  STL
+//
+//  Created by Havva Fatma Özbay on 22.06.2022.
+//
+
+#include <iostream>
+#include <vector>
+#include<string>
+
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+      if(strs.size() == 0){
+          return "";
+      }
+        string prefix = strs[0];
+        for(int i = 1; i<strs.size(); i++){
+            while(strs[i].find(prefix) != 0){
+                prefix = prefix.substr(0,prefix.size()-1);
+                if(prefix.size()== 0){
+                    return "";
+                }
+            }
+            
+        }
+        return prefix;
+    }
+};
+
+int main(){
+    Solution s;
+    vector<string> s1;
+    s1.push_back("flower");
+    s1.push_back("florasan");
+    s1.push_back("flat");
+    
+    cout << s.longestCommonPrefix(s1);
+ 
+}
